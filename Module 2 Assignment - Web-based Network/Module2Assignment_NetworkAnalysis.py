23da61601eefc07600e6e467733f76186e4ffe25
ramya_nataraj <ramyanataraj25@gmail.com>
2026-03-10 12:09:58 -0400

    Final code changes

--- Module 2 Assignment - Web-based Network/Module2Assignment_NetworkAnalysis.py
+++ Module 2 Assignment - Web-based Network/Module2Assignment_NetworkAnalysis.py
@@ -58,5 +58,33 @@ print("\n Top 20 characters by number of neighbors:")
 for node_id, deg in deg_list:
     print(f"{g.nodes[node_id].get('name', node_id)} - {deg}")
 
+# create a stacked bar graph that shows the positive & negative relationships
+# of the top ten characters
+# count relationships per character
+# combine all character appearances
+all_chars = pd.concat([df["source_name"], df["target_name"]])
 
+counts = all_chars.groupby(all_chars).size().sort_values(ascending=False).head(10)
+
+pos = []
+neg = []
+
+for name in counts.keys():
+    pos.append(df[((df["source_name"] == name) | (df["target_name"] == name)) & 
+                  (df["type"] == "+")].shape[0])
+    neg.append(df[((df["source_name"] == name) | (df["target_name"] == name)) & 
+                  (df["type"] == "-")].shape[0])
+    
+    # verify counts
+    print(name, pos, neg)
+
+plt.bar(counts.keys(), pos, label="Positive")
+plt.bar(counts.keys(), neg, bottom=pos, label="Negative")
+plt.xlabel("Characters")
+plt.ylabel("Relationships Counts split by Positive and Negative")
+plt.title("Top Characters by Relationships Types")
+
+plt.xticks(rotation=45)
+plt.legend()
+plt.show()
 
